04585d16a9c95a77c952af4b45e0340f273d8426
Vinicius Teixeira <viniciuscteixeira.rj@gmail.com>
2022-06-18 15:13:11 -0300

    fix

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import {Login} from './Pages/User/Login';
 import {Home} from './Pages/Home/Home';
+import {Categorias} from './Pages/Categoria/Categorias';
 
 export function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ export function AppRoutes() {
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/cadastro" element={<Login/>}/>
                 <Route path="/produtos" element={<Login/>}/>
+                <Route path="/categorias" element={<Categorias/>}/>
             </Routes>
         </BrowserRouter>
     );
