d138fb12d929a33ef5701b1f26405f3e0fc54558
Maurício Cruz1 <102329167+MauricioCruz1@users.noreply.github.com>
2022-06-20 17:25:07 -0300

    Register/cor de fundo do login

---
+++ src/Pages/Endereco/Edit.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import "../../Assets/Styles/CategoriaStyle.css"
+
+export const Edit = () =>{
+    const [nome, setNome] = useState("");
+    const [descricao, setDescricao] = useState("");
+
+    return (
+        <div className='container'>
+            <div className='container-login'>
+                <div className="wrap-login">
+                    <form className='login-form'>
+                        <div className="login-form-title">
+                            <h1>Editar</h1>
+                        </div>
+                        <div className='wrap-input'>
+                            <label className='focus-input' htmlFor="nome">Nome</label>
+                            <input className={nome !== "" ? "has-val input" : "input"} type="nome" name="nome"
+                                   id="nome" value={nome} onChange={(e) => setNome(e.target.value)}></input>
+                        </div>
+                        <div className="wrap-input">
+                            <label className='focus-input' htmlFor="descricao">Descrição</label>
+                            <input className={descricao !== "" ? "has-val input" : "input"} type="descricao"
+                                   name="descricao" id="descricao" value={descricao}
+                                   onChange={(e) => setDescricao(e.target.value)}></input>
+                        </div>
+                        <div className="ccontainer-login-form-btn">
+                            <button className='login-form-btn' type="submit">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,6 +4,7 @@ import {Login} from './Pages/User/Login';
 import Cadastro from './Pages/User/Cadastro';
 import {Home} from './Pages/Home/Home';
 import {Categorias} from './Pages/Categoria/Categorias';
+import {Endereco} from './Pages/Endereco/Endereco'
 
 export default function AppRoutes() {
     return (
@@ -11,9 +12,10 @@ export default function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/cadastro" element={<Cadastro/>}/>
+                <Route path="/register" element={<Cadastro/>}/>
                 <Route path="/produtos" element={<Login/>}/>
                 <Route path="/categorias" element={<Categorias/>}/>
+                <Route path="/endereco" element={<Endereco/>}/>
             </Routes>
         </BrowserRouter>
     );
