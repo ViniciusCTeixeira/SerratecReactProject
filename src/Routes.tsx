bd56e52d933eb26a653f2ea30c2739d1003c1db8
Vinicius Teixeira <viniciuscteixeira.rj@gmail.com>
2022-06-21 16:02:09 -0300

    Fix

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,17 +3,21 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './Pages/User/Login';
 import Cadastro from './Pages/User/Cadastro';
 import Home from './Pages/Home/Home';
-import Produtos from './Pages/Produtos/Produtos'
+import Produtos from './Pages/Produtos/Produtos';
+import Lancamentos from "./Pages/Produtos/Lancamentos";
 import Categorias from './Pages/Categoria/Categorias';
-import Endereco from './Pages/Endereco/Endereco'
+import Endereco from './Pages/Endereco/Endereco';
 import EditCategoria from './Pages/Categoria/Edit';
-import EditEndereco from './Pages/Endereco/Edit'
-import EditFuncionario from './Pages/Funcionario/Edit'
+import EditEndereco from './Pages/Endereco/Edit';
+import EditFuncionario from './Pages/Funcionario/Edit';
 import Funcionario from './Pages/Funcionario/Funcionario';
-import AddFuncionario from './Pages/Funcionario/Add'
-import AddCategoria from './Pages/Categoria/Add'
+import AddFuncionario from './Pages/Funcionario/Add';
+import AddCategoria from './Pages/Categoria/Add';
 import ByCategoria from "./Pages/Produtos/ByCategoria";
-import Cart from "./Pages/User/Cart"
+import Cart from "./Pages/User/Cart";
+import ListaProdutos from "./Pages/Produtos/ListaProdutos";
+import AddProduto from "./Pages/Produtos/Add";
+import EditProduto from "./Pages/Produtos/Edit";
 
 export default function AppRoutes() {
     return (
@@ -23,6 +27,7 @@ export default function AppRoutes() {
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Cadastro/>}/>
                 <Route path="/produtos" element={<Produtos/>}/>
+                <Route path="/lancamentos" element={<Lancamentos/>}/>
                 <Route path="/categorias" element={<ByCategoria/>}/>
                 <Route path="/cart" element={<Cart/>}/>
                 <Route path="/enderecos" element={<Endereco/>}/>
@@ -33,6 +38,9 @@ export default function AppRoutes() {
                 <Route path="/adm/categorias" element={<Categorias/>}/>
                 <Route path="/adm/categorias/edit" element={<EditCategoria/>}/>
                 <Route path="/adm/categorias/add" element={<AddCategoria/>}/>
+                <Route path="/adm/produtos" element={<ListaProdutos/>}/>
+                <Route path="/adm/produtos/edit" element={<EditProduto/>}/>
+                <Route path="/adm/produtos/add" element={<AddProduto/>}/>
             </Routes>
         </BrowserRouter>
     );
